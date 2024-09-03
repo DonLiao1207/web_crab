268f92291302e0102816f05ddc065502657c8f20
dliao <dliao@sram.com>
2024-09-03 17:58:04 +0800

    gpt genrate description

---
+++ parse_cherry_twins.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_pd = pd.read_excel(r'/home/sram-admin/workspace/web_crab/0902.xlsx', sheet_name=1)
+df_tb = pd.read_excel(r'/home/sram-admin/workspace/web_crab/0902.xlsx', sheet_name=2)
+df_alt = pd.read_excel(r'/home/sram-admin/workspace/web_crab/0902.xlsx', sheet_name=3)
